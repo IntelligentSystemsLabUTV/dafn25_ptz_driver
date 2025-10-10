dbac0609fe1507b1d2db1d401c788d1756c2b062
DanieleDiGimberardino <danieledigiambi@gmail.com>
2025-10-10 15:41:05 +0200

    test

--- src/ptz_camera_driver/src/ptz_camera_driver.cpp
+++ src/ptz_camera_driver/src/ptz_camera_driver.cpp
@@ -5,6 +5,6 @@ int main(int argc, char ** argv)
   (void) argc;
   (void) argv;
 
-  printf("hello world ptz_camera_driver package\n");
+  // printf("hello world ptz_camera_driver package\n");
   return 0;
 }
