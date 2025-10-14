cb52f8d62ac7c46d02866e7803454f320dc73579
Edose77e <edoardo.stati.es@gmail.com>
2025-10-14 12:38:13 +0200

    rimesso stampa hello world per check funzionamento ambiente

--- src/ptz_camera_driver/src/ptz_camera_driver.cpp
+++ src/ptz_camera_driver/src/ptz_camera_driver.cpp
@@ -6,6 +6,6 @@ int main(int argc, char ** argv)
   (void) argc;
   (void) argv;
   // modifica
-  // printf("hello world ptz_camera_driver package\n");
+  printf("hello world ptz_camera_driver package\n");
   return 0;
 }
